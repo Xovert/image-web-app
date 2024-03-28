41c51f1636d026f5693ae8dcaad67378dc5f8e3b
xovert <uxovert@gmail.com>
2024-03-28 17:23:20 +0700

    Deletion through modal done

--- imgwebapp/static/js/gallery.js
+++ imgwebapp/static/js/gallery.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $("#Error").modal('toggle');
 });
+
+$('#Deletion').on('show.bs.modal', (e)=>{
+    var data = $(e.relatedTarget).data('recordPath')
+    $('.btn-confirm').attr("formaction", data)
+})
